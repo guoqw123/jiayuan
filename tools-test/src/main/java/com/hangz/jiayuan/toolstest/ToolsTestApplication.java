29df24396d6c72295aadd581ce036031c9d2107a
guoqw <337693086@qq.com>
2020-12-23 13:25:03 +0800

    init

---
+++ tools-test/src/main/java/com/hangz/jiayuan/toolstest/ToolsTestApplication.java
@@ -0,0 +1,13 @@
+package com.hangz.jiayuan.toolstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsTestApplication.class, args);
+	}
+
+}
