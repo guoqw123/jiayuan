36cb7fb33c376429418add6bc67be3b4ae5bd610
guoqw <337693086@qq.com>
2020-12-23 13:29:23 +0800

    init

---
+++ tools-test/src/test/java/com/hangz/jiayuan/toolstest/ToolsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangz.jiayuan.toolstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
